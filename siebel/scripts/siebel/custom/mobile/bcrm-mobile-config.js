7aca68c6958b801583aa9dee1361025388e15cde
blacksheep IT consulting <alex@blacksheep-crm.com>
2021-01-17 14:31:31 +0100

    Update bcrm-mobile-config.js
    Removed "blacklist"

--- siebel/scripts/siebel/custom/mobile/bcrm-mobile-config.js
+++ siebel/scripts/siebel/custom/mobile/bcrm-mobile-config.js
@@ -1,5 +1,5 @@
 /*blacksheep IT consulting Copyright
-* Copyright (C) 2020
+* Copyright (C) 2016 - 2021
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
@@ -189,8 +189,8 @@ var BCRMBeautifierConf = {
 	"Contact Assoc Applet" : [".siebui-popup-action","rsl-hidden"]
 }
 
-//Exclusion list / blacklist for form applets that cannot be RWD'd
-var BCRMRWDBlacklist = [
+//Exclusion list for form applets that cannot be RWD'd
+var BCRMRWDExclusionList = [
     "Quote Catalog List Applet OUI Dropdown",
     "Layout Controls SI Applet",
     "Account Home Add Virtual Form Applet",
